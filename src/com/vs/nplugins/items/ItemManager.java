25cee7a7469e59503582c6f6a85e2269bc7e7c24
Vishwanath <vishwanath79@outlook.com>
2021-07-07 21:37:44 -0700

    recipes

--- src/com/vs/nplugins/items/ItemManager.java
+++ src/com/vs/nplugins/items/ItemManager.java
@@ -4,11 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import javax.naming.Name;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +43,18 @@ public class ItemManager {
         sr.setIngredient('B',Material.BLAZE_ROD);
         sr.setIngredient('S',Material.STICK);
         Bukkit.getServer().addRecipe(sr);
+
+        //Shapeless recipe
+        ShapelessRecipe sir = new ShapelessRecipe(NamespacedKey.minecraft("wand_shapeless"), item);
+        sir.addIngredient(2, Material.BLAZE_ROD);
+        sir.addIngredient(1,Material.DIAMOND);
+
+        Bukkit.getServer().addRecipe(sir);
+
+        //Furnace Recipe
+        FurnaceRecipe smelt = new FurnaceRecipe(NamespacedKey.minecraft("wand_smelt"),item,Material.EMERALD,1.0f,10*20); //10 seconds
+        Bukkit.getServer().addRecipe(smelt);
+
 
 
 
